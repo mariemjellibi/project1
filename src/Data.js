bffb3bb6e67e972c31db530ad932388c82da88f9
mariem <mariemjellibi4@gmail.com>
2023-08-24 13:44:33 +0200

    project1

---
+++ src/Data.js
@@ -0,0 +1,104 @@
+let shopItemsData = [
+    {
+      id: "jfhgbvnscs",
+      name: "CARGO PANTS",
+      price: 45,
+      page:"detailed1.html",
+      img: "images/img-1.jpg",
+      minfo:"click for more info"
+    },
+    {
+      id: "ioytrhndcv",
+      name: " AESTETIC DRESS",
+      price: 100,
+      page:"detailed2.html",
+      img: "images/img-14.avif",
+      minfo:"click for more info"
+    },
+    {
+      id: "wuefbncxbsn",
+      name: "T Shirt",
+      price: 25,
+      page:"detailed3.html",
+      img: "images/img-15.webp",
+      minfo:"click for more info"
+    },
+    {
+      id: "thyfhcbcv",
+      name: "WOMEN'S SHORTS",
+      price: 80,
+      page:"detailed4.html",
+      img: "images/img-16.jpg",
+      minfo:"click for more info"
+    },
+    {
+        id: "thyfhcbcz",
+        name: "women's blazer",
+        price: 200,        
+        img: "images/img-5.webp",
+        minfo:"click for more info"
+      },
+      {
+        id: "thyfhcbcvn",
+        name: "DRESS",
+        price: 100,        
+        img: "images/img-6.avif",
+        minfo:"click for more info"
+      },
+      
+      {
+        id: "thyfhcbcvp",
+        name: "DENIM SKIRT",
+        price: 70,
+        img: "images/img-7.webp",
+        minfo:"click for more info"
+      },
+      
+      {
+        id: "thyfhcbvu",
+        name: "WOMEN4S SHIRT",
+        price: 30,       
+        img: "images/img-8.webp",
+        minfo:"click for more info"
+      },
+      {
+        id: "thyfhcbvi",
+        name: "OVERSIZE HOODIE",
+        price: 10,
+        img: "images/img-9.webp",
+        minfo:"click for more info",
+        page:"detailed9.html"
+      },
+      {
+        id: "thyfhcbcvb",
+        name: "CUTE SHIRT ",
+        price: 15,      
+        img: "images/img-10.jpg",
+        minfo:"click for more info"
+      },
+      {
+        id: "thyfhcbcvr",
+        name: "LEGGINGS",
+        price: 25,       
+        img: "images/img-11.png",
+        minfo:"click for more info"
+      },
+      {
+        id: "thyfhcbcva",
+        name: "BAGGY JEANS",
+        price: 90,       
+        img: "images/img-12.jpg",
+        minfo:"click for more info"
+      },
+      {
+        id: "thyfhcbcvy",
+        name: "CROP TOP",
+        price:35,      
+        img: "images/img-13.jpg",
+        minfo:"click for more info"
+      },
+
+      
+      
+  ];
+  
